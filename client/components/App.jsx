f49ec9095ebda8e56b9dc37306e5bbb323dc7741
Kyle <kyle.rits.rodgers@gmail.com>
2017-07-05 09:04:14 -0400

    Installed React and React Components into our webpack setup.

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Oh, Hello World</h1>
+      </div>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -10,5 +18,6 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
